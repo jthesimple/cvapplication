a66aceda3053e9ec1549b932147f9e8c647d4fe3
Julio <julio.bermudez619@gmail.com>
2023-05-03 15:18:08 -0700

    refactored personal component

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,14 +1,126 @@
 import React, { Component } from "react";
-import Education from "./components/education.js";
-import Personal from './components/personal.js';
+///import Education from "./components/education.js";
+import PersonalInfo from './components/personal.js';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      personal: {
+        firstName: '',
+        lastName: '',
+        address: '',
+        phoneNumber: '',
+        email: '',
+      },
+      education: {
+        university: '',
+        degree:'',
+        subject:'',
+      },
+      eduList: [],
+      
+    };   
+
+  };
+
+  handleChangeFirst = (e) => {
+    this.setState({
+        personal: {firstName: e.target.value,
+                   lastName: '',
+                   address: '',
+                   phoneNumber: '',
+                   email: '',
+                  }
+                });
+  };
+
+  handleChangeLast = (e) => {
+    this.setState({
+      personal: {
+        firstName: this.state.personal.firstName,
+        lastName: e.target.value,
+        address: '',
+        phoneNumber: '',
+        email: '',
+      }
+    });
+  };
+
+  handleChangeAddress = (e) => {
+    this.setState({
+      personal: {
+        firstName: this.state.personal.firstName,
+        lastName: this.state.personal.lastName,
+        address: e.target.value,
+        phoneNumber: '',
+        email: '',
+      }
+    });
+  };
+
+  handleChangeNumber = (e) => {
+    this.setState({
+      personal: {
+        firstName: this.state.personal.firstName,
+        lastName: this.state.personal.lastName,
+        address: this.state.personal.address,
+        phoneNumber: e.target.value, 
+        email: '',
+      }
+    });
+  };
+
+  handleChangeEmail = (e) => {
+    this.setState({
+      personal: {
+        firstName: this.state.personal.firstName,
+        lastName: this.state.personal.lastName,
+        address: this.state.personal.address,
+        phoneNumber: this.state.personal.phoneNumber,
+        email: e.target.value,
+      }
+    });
+  };
+
+
   render() {
+    const {personal, education} = this.state;
     return (
     <>
       <div>Start creating you CV below:</div>
-      <Personal/>
-      <Education/>
+      <label>
+        <input onChange={this.handleChangeFirst} value={personal.firstName} placeholder='First Name'/>
+     </label>
+     <label>
+        <input onChange={this.handleChangeLast} value={personal.lastName} placeholder='Last Name'/>
+     </label>
+     <label>
+        <input onChange={this.handleChangeAddress} value={personal.address} placeholder='Address'/>
+     </label>
+     <label>
+        <input onChange={this.handleChangeNumber} value={personal.phoneNumber} placeholder='Phone Number'/>
+     </label>
+     <label>
+        <input onChange={this.handleChangeEmail} value={personal.email} placeholder='Email'/>
+     </label>
+     <hr></hr>
+     <div>Add your education below:</div>
+     <label>
+      <input onChange={this.handleChangeUni} value={education.university}placeholder='University/College'/>
+     </label>
+     <label>
+      <input onChange={this.handleChangeDegree} value={education.degree} placeholder='Degree'/>
+     </label>
+     <label>
+        <input onChange={this.handleChangeSubject} value={education.subject} placeholder='Subject'/>
+      </label>
+
+
+
+
+     <PersonalInfo personal={personal}></PersonalInfo>
     </>
     );
   }
--- cv-project/src/components/personal.js
+++ cv-project/src/components/personal.js
@@ -1,82 +1,17 @@
-import { Component } from 'react';
+import React from "react";
 
-class Personal extends Component {
-    constructor(){
-        super();
+const PersonalInfo = (props) => {
+    const {personal} = props;
 
-        this.state = {
-            firstName: '',
-            lastName: '',
-            address: '',
-            phoneNumber: '',
-            email: '',       
-        }
-
-    };
-
-    handleChangeFirst = (e) => {
-        this.setState({
-            firstName: e.target.value,
-        });
-    };
-
-    handleChangeLast = (e) => {
-        this.setState({
-            lastName: e.target.value,
-        });
-    };
-
-    handleChangeAddress = (e) =>{
-        this.setState({
-            address: e.target.value,
-        });
-    };
-    
-    handleChangeNumber = (e) =>{
-        this.setState({
-            phoneNumber: e.target.value,
-        });
-    };
-
-    handleChangeEmail = (e) =>{
-        this.setState({
-            email: e.target.value,
-        });
-    };
-
-
-
-
-    render() {
-        const {firstName, lastName, address, phoneNumber, email} = this.state;
-
-      return (<>
-      <label>
-        <input onChange={this.handleChangeFirst} value={firstName} placeholder='First Name'/>
-     </label>
-     <label>
-        <input onChange={this.handleChangeLast} value={lastName} placeholder='Last Name'/>
-     </label>
-     <label>
-        <input onChange={this.handleChangeAddress} value={address} placeholder='Address'/>
-     </label>
-     <label>
-        <input onChange={this.handleChangeNumber} value={phoneNumber} placeholder='Phone Number'/>
-     </label>
-     <label>
-        <input onChange={this.handleChangeEmail} value={email} placeholder='Email'/>
-     </label>
-     <hr></hr>
-
-     <h1>{firstName} {lastName} </h1>
-     <div>{address}</div>
-     <div>{phoneNumber}</div>
-     <div>{email}</div>
-     
-      </>
-      );
-    }
+    return (
+        <>
+        <h1>{personal.firstName} {personal.lastName}</h1>
+        <div>{personal.address}</div>
+        <div>{personal.phoneNumber}</div>
+        <div>{personal.email}</div>
+        </>
+    );
 }
 
 
-export default Personal;
+export default PersonalInfo;
